420ee9e4a92faa9063be066f43dba327cf38b914
Hilary Cheng <hilarycheng@gmail.com>
2013-05-26 10:34:06 +0800

    - Remove Unused Code

--- cangjie_native/jni/stroke_method.c
+++ cangjie_native/jni/stroke_method.c
@@ -18,106 +18,20 @@ int _mTotalMatch;
 void stroke_init(char *path)
 {
   stroke_func.mTotalMatch = 0;
-  
-  /*
-  int clear = 1;
-  int count = 0;
-  char key[8];
-  
-  stroke_func.mSaved = 0;
-  strncpy(stroke_func.mPath,         path, sizeof(stroke_func.mPath));
-  strncat(stroke_func.mPath, "/stroke.dat", sizeof(stroke_func.mPath));
-
-  for (count = 0; count < sizeof(stroke_index) / sizeof(jint); count++) {
-    stroke_index[count] = -1;
-  }
-
-  memset(key, 0, 8);
-  strcpy(key, "STROKE0");
-
-  FILE *file = fopen(stroke_func.mPath, "r");
-  if (file != 0) {
-    int read = fread(stroke_func.mBuffer, 1, sizeof(stroke_func.mBuffer), file);
-    if (memcmp(stroke_func.mBuffer, key, 8) == 0) {
-      int read = fread(stroke_frequency, 1, sizeof(stroke_frequency), file);
-      fclose(file);
-      if (read == sizeof(stroke_frequency)) clear = 0;
-    }
-  }
-   
-  if (clear != 0) {
-    for (count = 0; count < sizeof(stroke_frequency) / sizeof(jint); count++) {
-      stroke_frequency[count] = 0;
-    }
-  }
-  */
+  _mTotalMatch = 0;
 }
 
 int stroke_maxKey(void)
 {
-  return 51;
+  return STROKE_MAXKEY;
 }
 
 void stroke_searchWord(jchar key0, jchar key1, jchar key2, jchar key3, jchar key4)
 {
-  /*
-  int total = sizeof(stroke) / (sizeof(jchar) * 3);
-  int count = 0;
-  int loop  = 0;
-  int i = 0;
-  int j = 0;
-  int found = 0;
-  int offset = 0;
-
-  for (count = 0; count < sizeof(stroke_index) / sizeof(jint); count++) {
-    stroke_index[count] = 0;
-  }
-
-  for (count = 0; count < total; count++) {
-    if (key1 == 0) {
-      if (stroke[count][0] == key0) {
-	if (key1 == 0 && stroke[count][1] == 0) offset++;
-	stroke_index[loop] = count;
-	loop++;
-	found = 1;
-      } else if (found) {
-      	break;
-      }
-    } else {
-      if (stroke[count][0] == key0 && stroke[count][1] == key1) {
-	stroke_index[loop] = count;
-	loop++;
-	found = 1;
-      } else if (found) {
-      	break;
-      }
-    }
-  }
-
-  if (loop > 1) {
-    int swap = 1;
-    while (swap) {
-      swap = 0;
-      for (i = offset; i < loop - 1; i++) {
-	if (stroke_frequency[stroke_index[i]] < stroke_frequency[stroke_index[i + 1]]) {
-	  int temp = stroke_index[i];
-	  stroke_index[i] = stroke_index[i + 1];
-	  stroke_index[i + 1] = temp;
-	  swap = 1;
-	}
-      }
-    }
-  }
-
-  stroke_func.mTotalMatch = loop;
-  */
 }
 
 void stroke_searchWordMore(jchar *key0, jchar *key1, jchar *key2, jchar *key3, jchar *key4)
 {
-  /*
-  stroke_searchWord(key0[0], key1[0], key2[0], key3[0], key4[0]);
-  */
 }
 
 void stroke_searchWordArray(jchar *key, int len)
@@ -130,7 +44,6 @@ void stroke_searchWordArray(jchar *key, int len)
 
 jboolean stroke_tryMatchWordArray(jchar *key, int len)
 {
-  /* LOGE("Try Match Word : %d %d", key[0] - '1', len); */
   int index = 0, count;
   for (count = 0; count < len; count++) stroke_char[count] = key[count];
 
